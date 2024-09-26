2af6c36a4c0c1f75f71497cc98dd506a5df6c15c
Ulan Akmatbekov <ulan.akmatbekov@dfki.de>
2024-09-26 19:22:55 +0200

    Fixed the link error

--- Source/MMMIScene/Public/CommonTools.h
+++ Source/MMMIScene/Public/CommonTools.h
@@ -22,41 +22,40 @@
 //By default, multiplier is set to 0.01 (cm -> m, cm/s^2 -> m/s^2) for coordinates, lengths, forces,
 //but can take other values for other units conversion.
 //Should be 1 for dimensionless units i.e. scale or angular velocity.
-MMIStandard::MVector3 ToMVec3(const FVector& fVec, double multiplier = 0.01);
+MMISCENE_API MMIStandard::MVector3 ToMVec3(const FVector& fVec, double multiplier = 0.01);
 
 //Convert Unreal vector to MOSIM std::vector<double>
 //By default, multiplier is set to 0.01 (cm -> m, cm/s^2 -> m/s^2) for coordinates, lengths, forces,
 //but can take other values for other units conversion.
 //Should be 1 for dimensionless units i.e. scale or angular velocity.
-std::vector<double> ToDVec(const FVector& fVec, double multiplier = 0.01);
+MMISCENE_API std::vector<double> ToDVec(const FVector& fVec, double multiplier = 0.01);
 
 //Convert MOSIM vector to Unreal vector
 //By default, multiplier is set to 100 (m -> cm, m/s^2 -> cm/s^2) for coordinates, lengths, forces,
 //but can take other values for other units conversion.
 //Should be 1 for dimensionless units i.e. scale or angular velocity.
-FVector ToFVec3(const MMIStandard::MVector3& mVec, double multiplier = 100);
+MMISCENE_API FVector ToFVec3(const MMIStandard::MVector3& mVec, double multiplier = 100);
 
 //Convert MOSIM vector to Unreal vector
 //By default, multiplier is set to 100 (m -> cm, m/s^2 -> cm/s^2) for coordinates, lengths, forces,
 //but can take other values for other units conversion.
 //Should be 1 for dimensionless units i.e. scale or angular velocity.
-FVector ToFVec3(const std::vector<double>& dVec, double multiplier = 100);
+MMISCENE_API FVector ToFVec3(const std::vector<double>& dVec, double multiplier = 100);
 
-MMIStandard::MQuaternion ToMQuat(const FQuat& fquat);
-FQuat ToFQuat(const MMIStandard::MQuaternion& mquat);
+MMISCENE_API MMIStandard::MQuaternion ToMQuat(const FQuat& fquat);
 
-
+MMISCENE_API FQuat ToFQuat(const MMIStandard::MQuaternion& mquat);
 
 //Convert vector elements to Unreal 2D vector (XY plane) starting from specified index
 //By default, multiplier is set to 100 (m -> cm, m/s^2 -> cm/s^2) for coordinates, lengths, forces,
 //but can take other values for other units conversion.
 //Should be 1 for dimensionless units i.e. scale or angular velocity.
-FVector ToFVec2(const std::vector<double>& dVec, int startIndex, double multiplier = 100);
+MMISCENE_API FVector ToFVec2(const std::vector<double>& dVec, int startIndex, double multiplier = 100);
 
 //Convert vector elements to Unreal 3D vector starting from specified index
 //By default, multiplier is set to 100 (m -> cm, m/s^2 -> cm/s^2) for coordinates, lengths, forces,
 //but can take other values for other units conversion.
 //Should be 1 for dimensionless units i.e. scale or angular velocity.
-FVector ToFVec3(const std::vector<double>& dVec, int startIndex, double multiplier = 100);
+MMISCENE_API FVector ToFVec3(const std::vector<double>& dVec, int startIndex, double multiplier = 100);
 
 #pragma endregion
